87fff6c71a69024d803cb4393b9d0d83470376b0
Damar Buana Murti <damarbm30@gmail.com>
2022-07-21 20:48:00 +0700

    update data UMKM

--- src/components/beranda/Hero.jsx
+++ src/components/beranda/Hero.jsx
@@ -12,44 +12,45 @@ const Hero = () => {
             Sejarah Desa Gemawang jika dikaitkan dengan cerita masyarakat desa, kata gemawang berasal
             dari akronim <em>gema di awang-awang</em>. Desa Gemawang pada era dahulu telah terdengar
             atau kondang di mana-mana, dikarenakan menjadi salah satu percontohan desa vokasi di
-            Indonesia. Mulai dari kegiatan desa, potensi yang dimiliki, sampai keterampilan{" "}
-            <em>life skill</em> masyarakatnya
+            Indonesia
           </p>
           <p className="mt-6 mx-4 text-justify text-lg md:text-2xl">
-            Sejarah Desa Gemawang jika dikaitkan dengan cerita masyarakat desa
+            Desa Gemawang terdiri dari 7 dusun, yaitu Krajan, Banaran, Pitoro, Sekaja, Jlamprang, Kerep, dan Guyang Warak.
+            Di beberapa dusun yang ada tersebar berbagai UKM seperti makanan dan minuman, <em>fashion</em>, dan kerajinan.
+            Selain itu, di Desa Gemawang juga terdapat wisata budaya, edukasi, dan alam
           </p>
         </div>
-        <div className="mx-4 flex min-w-[280px] flex-col rounded-xl border border-slate-300 bg-zinc-200 py-2 text-center shadow-xl">
+        <div className="mx-4 flex min-w-[280px] flex-col rounded-xl border border-slate-300 bg-zinc-200 text-center shadow-xl">
           <div className="flex flex-col mx-auto my-auto items-center px-4">
             <h2 className="flex text-2xl md:text-3xl py-2 font-medium text-slate-500">
               <TerrainIcon style={{ fontSize: 40 }} />
               Geografi
               </h2>
             <div className="w-fit flex flex-col mx-auto">
-              <p className="flex items-center gap-1 px-4 py-1 text-slate-500 text-2xl">
+              <p className="flex items-center gap-1 px-4 py-1 text-slate-500 text-xl md:text-2xl">
                 Luas wilayah &#8594; 786 ha
               </p>
-              <p className="flex items-center gap-1 px-4 py-1 text-slate-500 text-2xl">
+              <p className="flex items-center gap-1 px-4 py-1 text-slate-500 text-xl md:text-2xl">
                 Ketinggian &#8594; 680 mdpl
               </p>
             </div>    
-            <div className="flex flex-col mx-auto my-auto items-center px-4">
+            <div className="flex flex-col mx-auto my-auto items-center px-4 mt-4 md:mt-12">
               <h2 className="flex text-2xl md:text-3xl py-2 font-medium text-slate-500">
                 <FenceIcon style={{ fontSize: 40}} />
                 Batas Wilayah
                 </h2>
             </div>
             <div className="w-fit flex flex-col mx-auto">
-                <p className="flex items-center gap-1 px-4 py-1 text-slate-500 text-2xl">
+                <p className="flex items-center gap-1 px-4 py-1 text-slate-500 text-xl md:text-2xl">
                   Utara &#8594; Pringsurat, Temanggung
                 </p>
-                <p className="flex items-center gap-1 px-4 py-1 text-slate-500 text-2xl">
+                <p className="flex items-center gap-1 px-4 py-1 text-slate-500 text-xl md:text-2xl">
                   Timur &#8594; Desa Bedono
                 </p>
-                <p className="flex items-center gap-1 px-4 py-1 text-slate-500 text-2xl">
+                <p className="flex items-center gap-1 px-4 py-1 text-slate-500 text-xl md:text-2xl">
                   Selatan &#8594; Grabag, Magelang
                 </p>
-                <p className="flex items-center gap-1 px-4 py-1 text-slate-500 text-2xl">
+                <p className="flex items-center gap-1 px-4 py-1 text-slate-500 text-xl md:text-2xl">
                   Barat &#8594; Pringsurat, Temanggung
                 </p>
               </div>
