31a0918f9a66e319f5c5a4d75e222ab6bdf6cc15
Damar Buana Murti <damarbm30@gmail.com>
2022-08-10 07:26:40 +0700

    update infografis on home

--- src/components/beranda/Map.jsx
+++ src/components/beranda/Map.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import WisataMap from "../../assets/img/Wisata.png";
+import Infografis from "../../assets/img/Infografis.png";
 
 const Map = () => {
   return (
     <main className="mt-12 pb-12">
-      <h1 className="mb-12 text-center text-3xl font-bold md:text-4xl">Lokasi dan Jalur Wisata</h1>
-      <img src={WisataMap} alt="" className="mx-auto flex max-w-xs rounded-xl md:max-w-4xl" />
-      <img src="/assets/designer-1.jpg" alt="" className="mx-auto flex max-w-xs md:max-w-4xl" />
+      <h1 className="mb-12 text-center text-3xl font-bold md:text-4xl">Lokasi dan Jalur Wisata Desa Gemawang</h1>
+      <img src={WisataMap} alt="" className="mx-auto mb-12 flex max-w-xs rounded-xl md:max-w-2xl" />
+      {/* <h1 className="mb-12 text-center text-3xl font-bold md:text-4xl">Lokasi Wisata dan UMKM Desa Gemawang</h1> */}
+      <img src={Infografis} alt="" className="mx-auto mb-4 flex max-w-xs rounded-xl md:max-w-4xl" />
     </main>
   );
 };
